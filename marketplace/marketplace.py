53616ecd06268ad232fb64355d7a3bac2302b1a7
Vergangenheit <lorenzo.ostano@gmail.com>
2022-01-28 13:21:36 +0100

    added dockerfiles

--- marketplace/marketplace.py
+++ marketplace/marketplace.py
@@ -20,5 +20,5 @@ def render_homepage():
     return render_template("homepage.html", recommendations=recommendations_response.recommendations)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
